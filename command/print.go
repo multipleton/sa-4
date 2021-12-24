5d81dfe34ac0864009551ad0aa288a567e7786f1
Yehor Bublyk <yehor.bk@gmail.com>
2021-12-24 21:53:25 +0200

    print command added

--- command/print.go
+++ command/print.go
@@ -1,5 +1,15 @@
 package command
 
-type Print struct{}
+import (
+	"fmt"
 
-// TODO
+	"github.com/multipleton/sa-4/engine"
+)
+
+type Print struct {
+	arg string
+}
+
+func (p *Print) Execute(loop *engine.Handler) {
+	fmt.Println(p.arg)
+}
