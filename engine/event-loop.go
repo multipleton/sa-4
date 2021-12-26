2ab9021b611bf1c0b748e5382160d0962445d6d5
olegmak02 <olleglion02@gmail.com>
2021-12-26 19:38:34 +0200

    Implemented MessageQueue and methods pull and push

--- engine/event-loop.go
+++ engine/event-loop.go
@@ -1,15 +1,43 @@
 package engine
 
-type EventLoop struct{}
+import "sync"
+
+type MessageQueue struct {
+	sync.Mutex
+	c []Command
+}
+
+func (q *MessageQueue) push(command Command) {
+	q.Lock()
+	q.c = append(q.c, command)
+	q.Unlock()
+}
+
+func (q *MessageQueue) pull() Command {
+	q.Lock()
+	cmd := q.c[0]
+	q.c[0] = nil
+	q.c = q.c[1:]
+	q.Unlock()
+	return cmd
+}
+
+type EventLoop struct {
+	queue *MessageQueue
+}
+
 
 func (el *EventLoop) Start() {
-	// TODO
+	el.queue = &MessageQueue{}
+	
+	// implement execution of commands
+	
 }
 
 func (el *EventLoop) Post(command Command) {
-	// TODO
+	el.queue.push(command)
 }
 
 func (el *EventLoop) AwaitFinish() {
-	// TODO
+	// implement method
 }
