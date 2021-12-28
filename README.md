a2df05a543e8719f6e9f18172ba0fd688e181aba
lightballer <bokiiartem@gmail.com>
2021-12-28 23:10:25 +0200

    update event loop, update readme

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # sa-4
+
 Software Architecture - Laboratory Work #4
+
+## How to run it?
+
+`From root directory run:`
+`go run ./cmd/server text.txt`
+`(or any path to input file instead of 'text.txt')`
--- engine/event-loop.go
+++ engine/event-loop.go
@@ -10,7 +10,7 @@ type MessageQueue struct {
 func (q *MessageQueue) push(command Command) {
 	q.Lock()
 	q.c = append(q.c, command)
-	q.Unlock()
+	defer q.Unlock()
 }
 
 func (q *MessageQueue) pull() Command {
@@ -18,7 +18,7 @@ func (q *MessageQueue) pull() Command {
 	cmd := q.c[0]
 	q.c[0] = nil
 	q.c = q.c[1:]
-	q.Unlock()
+	defer q.Unlock()
 	return cmd
 }
 
@@ -30,27 +30,30 @@ type EventLoop struct {
 	queue *MessageQueue
 	stopConfirm chan struct{}
 	stopRequest bool
+	finished bool
 }
 
 func (el *EventLoop) Start() {
 	el.queue = &MessageQueue{}
 	el.stopConfirm = make(chan struct{}, 1)
 	el.stopRequest = false
+	el.finished = false
 	go func() {
 		for {
-		  if (el.queue.isEmpty() && el.stopRequest) {
-				el.stopConfirm <- struct{}{}
-				return
-			} else if (!el.queue.isEmpty()) {
-				cmd := el.queue.pull()
-				cmd.Execute(el)
-			}
+		if (el.queue.isEmpty() && el.stopRequest) {
+			el.finished = true
+			el.stopConfirm <- struct{}{}
+			return
+		} else if (!el.queue.isEmpty()) {
+			cmd := el.queue.pull()
+			cmd.Execute(el)
 		}
+	}
 	}()
 }
 
 func (el *EventLoop) Post(command Command) {
-	if el.stopRequest {
+	if el.finished {
 		panic("queue is already closed.")
 	}
 	el.queue.push(command)
